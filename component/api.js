7b74610551208a98ac86b6b1ead3fcc583c453e2
Téo FIMINSKI <teo.fiminski@epsi.fr>
2023-10-17 11:17:14 +0200

    Modif Token

--- component/api.js
+++ component/api.js
@@ -1,6 +1,6 @@
 export default class API{
 
-    token = '2d86b4fdf3ee2150542c993efe89639b8f98ff853d72b57031e0fa5a2cc5d77d';
+    token = 'token';
 
     async get_seven_days_meteo(){
         const response = await fetch('https://api.meteo-concept.com/api/forecast/daily?insee=59350&world=false&end=6&token=' + this.token);
